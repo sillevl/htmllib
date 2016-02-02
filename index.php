d99a3342e10c14b9bff9a5bfa7aa21eaaa77cfea
Sille Van Landschoot <info@sillevl.be>
2016-02-02 11:05:28 +0100

    add title and paragraph

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Demo HTML-lib</title>
+  </head>
+  <body>
+    <h1>HTML Lib demo</h1>
+    <p>
+      hello world.
+    </p>
+  </body>
+</html>
